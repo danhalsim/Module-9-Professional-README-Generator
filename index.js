5f6b2684befeda635719908e191dbc81232bc4c4
dansim6935 <dansim6935@gmail.com>
2023-10-14 18:09:05 -0700

    Added questions for the user

--- index.js
+++ index.js
@@ -4,7 +4,66 @@ const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        message: "",
+        name: ""
+    },
+    
+    {
+        type: "input",
+        message: "What is the title of your project?",
+        name: "title"
+    },
+    {
+        type: "input",
+        message: "Provide a description of your project.",
+        name: "description"
+    },
+    {
+        type: "input",
+        message: "Provide installation instructions for your project.",
+        name: "installation"
+    },
+    {
+        type: "input",
+        message: "Provide usage information for your project.",
+        name: "usage"
+    },
+    {
+        type: "input",
+        message: "Provide contribution guidelines for your project.",
+        name: "contribution"
+    },
+    {
+        type: "input",
+        message: "Provide test instructions for your project.",
+        name: "test"
+    },
+    {
+        type: "list",
+        message: "Choose a license for your project:",
+        name: "license",
+        choices: [
+            "Apache License 2.0",
+            "GNU General Public License v3.0",
+            "MIT License",
+        ]
+    },
+    {
+        type: "input",
+        message: "What is your GitHub username?",
+        name: "username"
+    },
+    {
+        type: "input",
+        message: "What is your email address?",
+        name: "email"
+    },
+    
+    
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
